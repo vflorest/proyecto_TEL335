507b797249d2c255407cbc8bc16fd742d6878ff6
vicente_f <vicente.flores.t@gmail.com>
2024-05-14 19:47:01 -0400

    Declaración de ruta para requests

--- router/userRoutes.js
+++ router/userRoutes.js
@@ -1,21 +1,21 @@
 const express = require('express');
 const router = express.Router();
 
-// Endpoint para ir a la vista para ingresar un objeto perdido
+// Endpoint para ir a la vista para ingresar una nueva solicitud
 router.get('/new-request', (req, res) => {
-  // Lógica para ingresar un objeto perdido
+  // Lógica para ingresar una solicitud
     res.send('Respuesta desde user/new-request');
 });
 
-// Endpoint para ver la lista de objetos perdidos
+// Endpoint para ver la info del usuario
 router.get('/get-info', (req, res) => {
-    // Lógica para obtener y mostrar la lista de objetos perdidos
+    // Lógica para obtener y mostrar la info
     res.send('Respuesta desde user/get-info');
 });
 
-// Endpoint para ver la lista de solicitudes de usuarios
+// Endpoint para ver la lista de las solicitudes del usuario
 router.get('/prev-requests', (req, res) => {
-    // Lógica para obtener y mostrar la lista de solicitudes de usuarios
+    // Lógica para obtener y mostrar las solicitudes previas
     res.send('Respuesta desde user/prev-requests');
 });
 
