f897453def3ff67c3af4f9ddf078ac5bc6898bb2
vicente_f <vicente.flores.t@gmail.com>
2024-05-23 00:56:41 -0400

    Vista Login V1 Lista

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import Form from "./components/Form";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex w-full h-screen ">
+      <div className="w-full flex items-center justify-center lg:w-1/2">
+        <Form/>
+      </div>
+      <div className="hidden relative lg:flex items-center justify-center h-full w-1/2 bg-gray-200">
+        <div className="w-60 h-60 bg-gradient-to-tr from-violet-500 to-pink-500 rounded-full animate-bounce"/>
+        <div className="w-full h-1/2 absolute bottom-0 bg-white/10 backdrop-blur-lg"/>
+      </div>
     </div>
   );
 }
